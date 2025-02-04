4654efc9426ec4a81701d02c68fd05686ff10582
Artur Markiewicz <artur.markiewicz@luy.eu>
2025-02-04 15:11:09 +0100

    Doing evil stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'sonar-test';
+
+  tITLE = "I'm an evil title string!";
+
+  doIt() {
+    if (true) {
+      console.log("I'm doing it!");
+    }
+
+    // TODO: Remove this
+  }
 }
